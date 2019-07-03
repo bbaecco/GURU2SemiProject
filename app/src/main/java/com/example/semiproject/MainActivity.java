31c46f28d5db1983c906d89eff9e5bec6fa71562
KimJiYeon <kwldusrnclf@naver.com>
2019-07-03 15:55:41 +0900

    세미 파이널 디테일한 내용

--- app/src/main/java/com/example/semiproject/MainActivity.java
+++ app/src/main/java/com/example/semiproject/MainActivity.java
@@ -11,10 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //테스트 코드1
-        //소스도 수정
-
-
-        //커밋 및 push 테스트
+        // 로그인 실패 메세지 : 다이얼로그 또는 토스트
+        // 회원이 아닌데 로그인 : 회원이 아닙니다
+        //리스트를 제이슨으로 저장하고 뽑고
+        //사진이 찍힌 경로를 잘 저장하고 있어야 함
+        //회원가입한 사람의 메모가 다 보여야 함
+        //위치 정보는 아직 안배워서 안해도 됨
+        //사진을 안찍으면 저장이 되면 안됨
+        //메모가 완성이 될 때마다 리스트뷰가 완성되야 한다
+        //상세 보기 화면 > textview 아니고 edittext로 하기
+        //삭제할 땐 삭제하시겠습니까? 물어보고 삭제하기
+        
     }
 }
