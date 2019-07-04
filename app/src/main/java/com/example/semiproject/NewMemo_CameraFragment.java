f61d8ab70f868c412d664e6dfa42f5b2f9c32c4b
KimJiYeon <kwldusrnclf@naver.com>
2019-07-04 11:32:29 +0900

    수정, 상세보기 버튼 눌렀을 때 나오는 메모 상세보기 탭 추가

--- app/src/main/java/com/example/semiproject/NewMemo_CameraFragment.java
+++ app/src/main/java/com/example/semiproject/NewMemo_CameraFragment.java
@@ -3,6 +3,8 @@ package com.example.semiproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewMemo_CameraFragment extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class NewMemo_CameraFragment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_memo__camera_fragment);
+
+        //버튼 클릭시 카메라 작동
+        Button newMemoBtnCamera = findViewById(R.id.NewMemoBtnCamera);
+        newMemoBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }
