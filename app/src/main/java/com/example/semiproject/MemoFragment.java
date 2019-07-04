0fd5e776d70d8a0d11880dc2612828bcfc1a88ca
KimJiYeon <kwldusrnclf@naver.com>
2019-07-04 10:22:54 +0900

    view_memo.xml추가(리스트뷰에 보일 화면 레이아웃)

--- app/src/main/java/com/example/semiproject/MemoFragment.java
+++ app/src/main/java/com/example/semiproject/MemoFragment.java
@@ -3,12 +3,29 @@ package com.example.semiproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MemoFragment extends AppCompatActivity {
+
+    Button memoBtnAddMemo;
+    ListView memoListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_fragment);
+
+        memoBtnAddMemo = findViewById(R.id.MemoBtnAddMemo);
+        memoListView = findViewById(R.id.MemoListView);
+
+        //버튼 클릭시 메모 리스트 추가
+        memoBtnAddMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }
